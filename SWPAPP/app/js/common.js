fe6dce1e4515f2983ba01aa6705ad88a7358eccc
sergeymad <serhiohejo@gmail.com>
2017-07-08 12:35:53 +0300

    SwitFarb_Com

---
+++ SWPAPP/app/js/common.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function() {
+
+	// Custom JS
+    $(".upbutton").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+
+
+    //logos.wrap('<div class="opened"></div>');
+
+        if($(window).width() <= '768'){
+            $(".logos .image:gt(9)").addClass('closed').css("display","none");
+            $(".more p").on('click' ,function () {
+                $(".closed").slideToggle();
+                $(".closed").toggleClass("on");
+                if($(".closed").hasClass("on")){
+                    $(this).text("Сховати");
+                }
+                else{
+                    $(".closed").removeClass("on");
+                    $(".more p").text("Та інші...");
+                }
+            });
+        }
+
+
+    //nav
+    $(".nav-trigger").click(function(){
+        if($(this).hasClass("is-active")){
+            $(this).removeClass("is-active");
+        }else{
+            $(this).addClass("is-active");
+        }
+        $(".top_nav_mob").slideToggle(500);
+    });
+
+
+    //$(window).load(deleting());
+});
